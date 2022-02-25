93562719cb7ade8b96a0b0b8a1e0d31997bb3d67
esears0657 <97701405+esears0657@users.noreply.github.com>
2022-02-24 22:35:30 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+var addWords = (array) => {
+    for (var i = 1; i < array.length; i++) {
+      console.log('The runner ups are ' +array[i] );
+    }
+}//for loop that diplays each thing and the text after, does it until it reaches the array length
+ 
+console.log('The best time is 1999');
+var bestTimes = ["1999","2010","2015"];
+addWords(bestTimes);
+//returns
